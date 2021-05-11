ff550c65729bbca6e419fb9f41887336dc7c5f1a
0306181100 <0306181100@caothang.edu.vn>
2021-05-11 15:38:24 +0700

    update slide show trang chủ

--- QuanLiRapPhim/wwwroot/View/font-end/Index/controller.js
+++ QuanLiRapPhim/wwwroot/View/font-end/Index/controller.js
@@ -16,15 +16,45 @@ app.config(function ($routeProvider) {
 
 app.controller('index', function ($scope) {
     $scope.mvs = [];
-    for (var i = 0; i < 7; i++){
+    for (var i = 0; i < 7; i++) {
         $scope.mvs[i] = {
             id: i,
             img: 'https://www.galaxycine.vn/media/c/o/cogai_2.jpg',
             trailer: 'https://www.youtube.com/embed/LggaymnzDjc'
         };
-
     }
     $scope.play = function (stt) {
         $scope.trailer = $scope.mvs[stt].trailer;
+    }
+
+
+    $scope.init = function () {
+        $scope.viewedSlider = $('.bbb_viewed_slider');
+        $scope.viewedSlider.owlCarousel({
+            loop: true,
+            margin: 280,
+            autoplay: true,
+            autoplayTimeout: 6000,
+            dots: false,
+            responsive:
+            {
+                0: { items: 1 },
+                575: { items: 2 },
+                768: { items: 3 },
+                991: { items: 4 },
+                1199: { items: 6 }
+            }
+        });
+        
+    }
+    setTimeout(function () {
+            $scope.init();
+    }, 1000);
+    
+    $scope.prev = function () {
+        $scope.viewedSlider.trigger('prev.owl.carousel');
+    }
+    $scope.next = function () {
+        $scope.viewedSlider.trigger('next.owl.carousel');
     }
 });
