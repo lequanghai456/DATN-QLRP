d931827fcb619f485ee3b42fb96eb8e2de1f5466
0306181100 <0306181100@caothang.edu.vn>
2021-05-13 13:43:48 +0700

    cắt layout trang review movie, movie detail

---
+++ QuanLiRapPhim/wwwroot/View/front-end/movie_review/controller.js
@@ -0,0 +1,27 @@
+﻿var ctxfolderurl = "/View/front-end/about";
+
+var app = angular.module('App', ['ngRoute']);
+
+app.controller('Ctroller', function () {
+
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: ctxfolderurl + '/index.html',
+            controller: 'index'
+        }).when('/:NameMovie', {
+            templateUrl: ctxfolderurl + '/moviedetail.html',
+            controller: 'moviedetail'
+        })
+});
+
+app.controller('index', function ($scope) {
+    
+    
+});
+
+app.controller('moviedetail', function ($scope) {
+
+});
