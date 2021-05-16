84e05cfe6fa3a59704875ce0d4a2eba848e44a28
lequanghai456 <haiquangle456@gmail.com>
2021-05-16 12:39:45 +0700

    Kết nối database

---
+++ QuanLiRapPhim/Areas/Admin/Data/DPContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QuanLiRapPhim.Areas.Admin.Models;
+namespace QuanLiRapPhim.Areas.Admin.Data
+{
+    public class DPContext : DbContext
+    {
+        public DPContext(DbContextOptions<DPContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
