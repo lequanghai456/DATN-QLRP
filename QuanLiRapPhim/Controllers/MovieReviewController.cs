1d03e3db7a19db06087b3185e57ffe1a054b9de3
0306181100 <0306181100@caothang.edu.vn>
2021-05-24 12:19:18 +0700

    api đổ dữ liệu ảo lên trang ds phim

--- QuanLiRapPhim/Controllers/MovieReviewController.cs
+++ QuanLiRapPhim/Controllers/MovieReviewController.cs
@@ -12,5 +12,41 @@ namespace QuanLiRapPhim.Controllers
         {
             return View();
         }
+        List<movie> movies;
+        public MovieReviewController()
+        {
+            movies = new List<movie>();
+            for (int i = 1; i < 9; i++)
+            {
+                movie movie = new movie
+                {
+                    Id = i,
+                    Title = "Maleficient",
+                    Poster = "thumb-" + i + ".jpg",
+                    Describe = "Sed ut perspiciatis unde omnis iste natus error voluptatem doloremque.",
+                    Status = 0,
+                };
+                movies.Add(movie);
+            }
+        }
+
+        public JsonResult GetItem(int id)
+        {
+            return Json(movies.FirstOrDefault(movie=>movie.Id==id));
+        }
+        public async Task<JsonResult> GetMovie()
+        {            
+            return Json(movies);
+        }
+    }
+    class movie
+    {
+        public int Id { get; set; }
+        public String Title{ get; set; }
+        public String Trailer{ get; set; }
+        public String Poster{ get; set; }
+        public String Describe{ get; set; }
+        public int Status{ get; set; }
+        public DateTime Time{ get; set; }
     }
 }
