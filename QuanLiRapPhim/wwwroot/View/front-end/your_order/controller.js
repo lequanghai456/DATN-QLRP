144d8d4eb62e86750b679cacd4fa98fb143e5397
0306181100 <0306181100@caothang.edu.vn>
2021-05-31 21:10:54 +0700

    modal cho phần chọn dịch vụ, chưa có template

--- QuanLiRapPhim/wwwroot/View/front-end/your_order/controller.js
+++ QuanLiRapPhim/wwwroot/View/front-end/your_order/controller.js
@@ -1,6 +1,6 @@
 ﻿var ctxfolderurl = "/View/front-end/your_order";
 
-var app = angular.module('App', ['ngRoute']);
+var app = angular.module('App', ['ui.bootstrap', 'ngRoute', 'ngAnimate']);
 
 app.controller('Ctroller', function () {
 
@@ -14,10 +14,32 @@ app.config(function ($routeProvider) {
         })
 });
 
-app.controller('index', function ($scope) {
+app.controller('index', function ($scope, $uibModal) {
     $scope.init = function () {
         $("#my_order").addClass("current-menu-item");
     }
     $scope.init();
+    $scope.choseService = function () {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: ctxfolderurl + "/chose-service.html",
+            controller: "choseService",
+            size: 'xl'
+        });
+
+        modalInstance.result.then(function (res) {
+            alert(res);
+        });
+    }
+});
+app.controller('choseService', function ($scope, $uibModalInstance, $uibModal) {
+
+    $scope.cancel = function () {
+        $uibModalInstance.close('cancel');
+    }
+
+    $scope.payment = function () {
+        $uibModalInstance.close('ok');
+    }
 
 });
