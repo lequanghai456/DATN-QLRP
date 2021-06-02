42e94a7245d11644e7354e5e4c4bf319694abd05
lequanghai456 <haiquangle456@gmail.com>
2021-06-02 17:56:57 +0700

    update claim identity roles

--- QuanLiRapPhim/Areas/Admin/Data/CustomUserClaimsPrincipalFactory .cs
+++ QuanLiRapPhim/Areas/Admin/Data/CustomUserClaimsPrincipalFactory .cs
@@ -11,21 +11,21 @@ namespace QuanLiRapPhim.Areas.Admin.Data
 {
     public class CustomUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<Staff, Role>
     {
-        public CustomUserClaimsPrincipalFactory(UserManager<Staff> userManager, RoleManager<Role> roleManager, IOptions<IdentityOptions> options) : base(userManager, roleManager, options)
+        public CustomUserClaimsPrincipalFactory(UserManager<Staff> userManager, IdentityContext context, RoleManager<Role> roleManager, IOptions<IdentityOptions> options) : base(userManager, roleManager, options)
         {
+            _context = context;
         }
+        private readonly IdentityContext _context;
         protected override async Task<ClaimsIdentity> GenerateClaimsAsync(Staff user)
         {
             var identity = await base.GenerateClaimsAsync(user);
 
             identity.AddClaim(new Claim("FullName", user.FullName));
             identity.AddClaim(new Claim("RoleId", user.RoleId.ToString()));
-            var roles = await UserManager.GetRolesAsync(user);
-            foreach (var role in roles)
-            {
-                identity.AddClaim(new Claim(ClaimTypes.Role, role));
-                identity.AddClaim(new Claim("Role", role));
-            }
+            var roles = _context.Roles.FirstOrDefault(x=>x.Id == user.RoleId);
+
+            identity.AddClaim(new Claim(ClaimTypes.Role, roles.Name));
+            identity.AddClaim(new Claim("Role", roles.Name));
             return identity;
         }
     }
