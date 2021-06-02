b577c32c1686f4efdae91cce9bae192c932e747f
lequanghai456 <haiquangle456@gmail.com>
2021-06-02 12:42:51 +0700

    create and edit, jtable chưa xong

---
+++ QuanLiRapPhim/Areas/Admin/Controllers/RoomsController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuanLiRapPhim.Areas.Admin.Data;
+using QuanLiRapPhim.Areas.Admin.Models;
+
+namespace QuanLiRapPhim.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoomsController : Controller
+    {
+        private readonly IdentityContext _context;
+
+        public RoomsController(IdentityContext context)
+        {
+            _context = context;
+        }
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+          
+            ViewBag.List = _context.Rooms.Include(r => r.Staff);
+            base.OnActionExecuted(context);
+        }
+        public IActionResult Index(int? id)
+        {
+            Room room = null;
+            if(id!=null)
+            {
+                room = _context.Rooms.FirstOrDefault(s => s.Id == id);
+            }
+            var identityContext = _context.Rooms.Include(r => r.Staff);
+            ViewData["IdStaf"] = new SelectList(_context.Staffs, "Id", "FullName");
+            return View(room);
+        }
+        //Danh sach phong json
+        public  JsonResult JsonRoom()
+        {
+            var data =  _context.Rooms.Include(r=>r.Staff).ToList();
+            return Json(data);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Row,Col,IdStaftManager")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(room);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdStaftManager"] = new SelectList(_context.Staffs ,"Id", "FullName", room.IdStaftManager);
+            return View(room);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Row,Col,IdStaftManager")] Room room)
+        {
+            if (id != room.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(room);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoomExists(room.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            ViewData["IdStaftManager"] = new SelectList(_context.Staffs, "Id", "FullName", room.IdStaftManager);
+            return RedirectToAction(nameof(Index));
+        }
+        private bool RoomExists(int id)
+        {
+            return _context.Rooms.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ QuanLiRapPhim/Migrations/20210601105455_add_isdelete.cs
@@ -0,0 +1,200 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuanLiRapPhim.Migrations
+{
+    public partial class add_isdelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "Tickets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "ShowTimes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "Sevices",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "Rooms",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "Rates",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "Movies",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "Macs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "Devices",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "Comments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "Bills",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "BillDetails",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetRoles",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isdelete",
+                table: "AspNetRoles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "ShowTimes");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "Sevices");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "Rates");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "Macs");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "Bills");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "BillDetails");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "isdelete",
+                table: "AspNetRoles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetRoles",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+        }
+    }
+}
