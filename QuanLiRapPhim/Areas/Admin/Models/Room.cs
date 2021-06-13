13effbaae5c0c86e970bff6093aa0245ee425d0d
lequanghai456 <haiquangle456@gmail.com>
2021-06-13 10:38:29 +0700

    update classdiagram

--- QuanLiRapPhim/Areas/Admin/Models/Room.cs
+++ QuanLiRapPhim/Areas/Admin/Models/Room.cs
@@ -14,9 +14,6 @@ namespace QuanLiRapPhim.Areas.Admin.Models
         public String Name{get; set;}
         public int Row{get; set;}
         public int Col{get; set;}
-        public int? RoomId { get; set; }
-        [ForeignKey("RoomId")]
-        public virtual Room Rooms { get; set; }
         public virtual ICollection<ShowTime> LstShowTime { get; set; }
         public virtual ICollection<Device> Devices { get; set; }
         public bool IsDelete { get; set; }
