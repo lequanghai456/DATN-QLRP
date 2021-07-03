d2d4f6787e158509722423a0b8d2c760a7996222
lequanghai456 <haiquangle456@gmail.com>
2021-07-03 11:44:19 +0700

    update model_seat

--- QuanLiRapPhim/Areas/Admin/Models/Seat.cs
+++ QuanLiRapPhim/Areas/Admin/Models/Seat.cs
@@ -13,6 +13,7 @@ namespace QuanLiRapPhim.Areas.Admin.Models
         public int Y{get;set;}
         public decimal ExtraPrice { get; set; }
         public int RoomId { get; set; }
+        public int Status { get; set; }
         [ForeignKey("RoomId")]
         public virtual Room Room { get; set; }
         public virtual ICollection<Ticket> Ticket { get; set; }
